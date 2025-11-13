e11531998ae91dc4b278de83300c996b81b246a2
Sri Phani Deverakonda <phanidatta0406@gmail.com>
2025-11-13 14:21:23 +0100

    final

--- bWAPP/config.inc.php
+++ bWAPP/config.inc.php
@@ -18,11 +18,16 @@ bWAPP is licensed under a Creative Commons Attribution-NonCommercial-NoDerivativ
 
 include("admin/settings.php");
 
-// Connection settings
-$server = $db_server;
-$username = $db_username;
-$password = $db_password;
-$database = $db_name;
+// // Connection settings
+// $server = $db_server;
+// $username = $db_username;
+// $password = $db_password;
+// $database = $db_name;
+
+$db_server   = "db";
+$db_database = "bwapp";
+$db_username = "bee";
+$db_password = "bug";
 
 // Connection settings, used in older bWAPP versions
 // $server = "localhost";
